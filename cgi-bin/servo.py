64793d09e951241505610b4b408b60d94919a394
Ryuya19 <s16025@tokyo.kosen-ac.jp>
2020-01-14 10:52:18 +0900

    修正しました

--- cgi-bin/servo.py
+++ cgi-bin/servo.py
@@ -1,20 +1,19 @@
 import time
 import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(2, GPIO.OUT)
-
 def setservo(deg):
-    GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
     servo = GPIO.PWM(2, 50)
-    servo.ChangeDutyCycle(2.5)
+#    servo.ChangeDutyCycle(2.5)
+    servo.start(0.0)
     servo.ChangeDutyCycle(((1.9 / 180) * deg + 0.5) * 5)
     time.sleep(1)
-    GPIO.cleanup()
+#    GPIO.cleanup()
 
-
+"""
 for i in range(10):
     setservo(-90)
     setservo(90)
 
-
+"""
--- cgi-bin/webservo.py
+++ cgi-bin/webservo.py
@@ -6,22 +6,28 @@ import time
 import RPi.GPIO as GPIO
 import servo    #servo movement
 
-print('Content-type: text/html; charset=UTF-8\r\n')
+print('Content-type: text/html; charset=UTF-8\r\n\r\n')
 print('Web Servo')
 
 print('<form action="" method="post">')
 print('input degrees (-90 ~ 90)')
 print('<br>')
-print('<input type="text" name="deg">')
-print('<input type="submit" value="send">')
+print('<input type="number" name="deg">')
+print('<input type="submit" name="sub" value="send">')
 print('</form>')
 
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(14, GPIO.OUT)
-GPIO.cleanup()
+#GPIO.setmode(GPIO.BCM)
+#GPIO.cleanup()
+#GPIO.setup(14, GPIO.OUT)
 
 form = cgi.FieldStorage()
-value = int(form.getvalue("deg"))
-servo.setservo(value)
-print('SERVO TURNING FOR ' + value)
+value = form.getvalue("sub")
+
+
+### add
+if value == "send":
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(2, GPIO.OUT)
+    servo.setservo(int(form.getvalue("deg")))
+
